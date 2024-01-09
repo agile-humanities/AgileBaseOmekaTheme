4d1e71917377ea0e08898ab8308e62d49b1f602d
Bill Kennedy <bill@agilehumanities.ca>
2024-01-09 16:18:39 -0500

    UX/UI Improvements
    - Improved identity positioining for portrait-ratio logos
    - Added ability to set which DOM elements apply image aspect ratios in local script
    - Better typesetting on slide captions
    - Small refactor of slide viewer SASS files for clarity
    - Other small tweaks

---
+++ source/js/00_omekaGeneral.js
@@ -0,0 +1,11 @@
+/**
+ *  @file 00_omekaGeneral.js
+ *
+ *  Modifications to the PTD form elements
+ *
+ */
+
+
+
+// Apply aspect ratio attributes to images in the following containers. Works with imageRatio.js in themesystem core.
+const imageARScope = 'body > header, body > footer, main, #splash, #title';
