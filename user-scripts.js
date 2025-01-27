263a8e896acdee6d21108d995553da6c1d2e3450
Bill Kennedy <bill@agilehumanities.ca>
2025-01-27 10:55:21 -0500

    Adding CSS and JS files for devs not using the build system. Other tweaks

---
+++ user-scripts.js
@@ -0,0 +1,8 @@
+/**
+ *  @file user-scripts.js
+ *  @description A file for front-end developers to add customized javascript without rebuilding the asset folder
+ *    from source. The “asset” folder is volatile, meaning that it gets deleted and rebuilt when the theme assets
+ *    are rebuilt using gulp. Moreover, the JavaScript is compiled and difficult to work with.
+ *    Use this file if you are not intending to use a gulp-based theme development workflow.
+ *
+ */
