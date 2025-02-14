4f8d6ea7b639c7d944997e2dc0165e491613e55d
Bill Kennedy <bill@agilehumanities.ca>
2025-02-13 19:51:29 -0500

    Built assets

--- asset/js/default.js
+++ asset/js/default.js
@@ -460,6 +460,71 @@ if (typeof Drupal !== "undefined") {
   })(jQuery);
 }
 
+// add the "has-three-cards" class in the featured exhibits block
+// when there are three attachments
+(function($) {
+  $(document).ready(function(){
+    const featuredExhibitsBlock = $('.site-link-cards');
+
+    if (featuredExhibitsBlock.length) {
+      featuredExhibitsBlock.each(function() {
+        const NUMBER_OF_ATTACHMENTS = 3;
+        const cards = $(this).children('.link-card');
+
+        if (cards.length === NUMBER_OF_ATTACHMENTS) {
+          $(this).addClass('has-three-cards');
+        }
+      });
+    }
+
+    const scrollDownBtn = $('.scroll-down-btn');
+
+    scrollDownBtn.on('click', function() {
+      $('html, body').animate({
+        scrollTop: featuredExhibitsBlock.offset().top
+      });
+    });
+
+
+  });
+})(jQuery);
+
+// Adds "slide-full-screen" class to the slideshow on click of the full screen button
+(function($) {  
+  $(document).ready(function() {
+    const slideshow = $('.slideshow');
+    
+    if (slideshow.length) {
+      // function that takes the browser specific full screen event 
+      // and document property to access the element in full screen mode
+      function changeSlideToFullScreen(fullScreenEvent, fullScreenElementProperty) {
+        $(document).on(fullScreenEvent, function(){
+          const slickTrack = $('.slick-track');
+          
+          if (document[fullScreenElementProperty]) {
+            const slideshowIsFullscreen = document[fullScreenElementProperty].classList.contains('slideshow');
+            
+            if (slideshowIsFullscreen) {
+              slideshow.slick('reinit');
+              slickTrack.addClass('slide-fullscreen');
+            };
+          };
+          
+          if (!document[fullScreenElementProperty]) {
+            slickTrack.removeClass('slide-fullscreen');
+            slideshow.slick('reinit');
+          };
+        });
+      };
+
+      changeSlideToFullScreen('fullscreenchange', 'fullscreenElement');
+      // for safari fullscreen
+      changeSlideToFullScreen('webkitfullscreenchange', 'webkitFullscreenElement');
+   
+    };
+
+  });  
+})(jQuery);
 (function($) { 
   $(document).ready(function() {
 
